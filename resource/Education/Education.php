f78cf25b1153c065933ee0b297358e17d2ed3ae9
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-12-27 10:12:16 +0700

    Delete

--- resource/Education/Education.php
+++ resource/Education/Education.php
@@ -14,14 +14,14 @@
 <?php 
 if (isset($_GET["ma"])) {
     $EducationId = $_GET["ma"];
-    mysqli_query($conn, "DELETE FROM `education` WHERE EducationId=$EducationId");
+mysqli_query($conn, "DELETE FROM `education` WHERE EducationId={$EducationId}");
 }
 ?>
 <?php
 if (isset($_POST['btnDelete']) && isset($_POST['checkbox'])) {
     for ($i = 0; $i < count($_POST['checkbox']); $i++) {
         $EducationId1 = $_POST['checkbox'][$i];
-        mysqli_query($conn, "DELETE FROM `education` WHERE EducationId=$EducationId1");
+    mysqli_query($conn, "DELETE FROM `education` WHERE EducationId={$EducationId1}");
     }
 }
 ?>
@@ -36,16 +36,16 @@ if (isset($_POST['btnDelete']) && isset($_POST['checkbox'])) {
 			</a>
 
 		</p>
-		<table class="table-striped table-responsive table-bordered" id="myTable">
+		<table class="table-striped table-responsive table-bordered" id="myTable" border='1'>
 			<thead>
 				<tr>
-					<th><strong>Choice</strong></th>
+					<th ><strong>Choice</strong></th>
 					<th ><strong>No</strong></th>
-					<th class="col-2"><strong>Education</strong></th>
-                    <th class="col-4"><strong>Name</strong></th>
-                    <th class="col-2"><strong>Details</strong></th>
-					<th class="col-2"><strong>Delete</strong></th>
-					<th class="col-2"><strong>Update</strong></th>
+					<th ><strong>Education</strong></th>
+                    <th ><strong>Name</strong></th>
+                    <th class="col-8"><strong>Details</strong></th>
+					<th class="col-1"><strong>Delete</strong></th>
+					<th class="col-1"><strong>Update</strong></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -55,17 +55,17 @@ if (isset($_POST['btnDelete']) && isset($_POST['checkbox'])) {
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         ?>
 					<tr>
-						<td><input name="checkbox[]" type="checkbox" id="checkbox[]"  class="form-control" value="<?php echo $row["GradeId"] ?>"></td>
+						<td><input name="checkbox[]" type="checkbox" id="checkbox[]"  class="form-control" value="<?php echo $row["EducationId"] ?>"></td>
 						<td><?php echo $num ?></td>
 						<td><?php echo $row["EducationId"] ?></td>
                         <td><?php echo $row["EducationName"] ?></td>
                         <td><?php echo $row["EducationDetails"] ?></td>
 						<td align='center'>
-							<a class="btn btn-default"   href="?page=education&ma=<?php echo $row['EducationId']; ?>" onclick="return deleteConfirm()">
-								<i class="fa fa-remove"></i></a>
+							<a class="btn btn-default"   href="?page=edu&ma=<?php echo $row['EducationId']; ?>" onclick="return deleteConfirm()">
+								Delete</a>
 							</td>
 							<td>
-								<a class="btn btn-default" href="?page=educationUpdate&ma=<?php echo $row['EducationId']; ?>"><i class="fa fa-share"></i></a>
+								<a class="btn btn-default" href="?page=educationUpdate&ma=<?php echo $row['EducationId']; ?>">Update</a>
 							</td>
 						</tr>
 						<?php
